3d640e0ba2862b504ccab901c9d9a9cd4d175534
bek1F <xamidullayevbehruz1@gmail.com>
2026-04-07 16:36:14 +0500

    КВКНОВОНКВОНКВЛНВ

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,12 +1,63 @@
+import './style.css';
 
-import './style.css'
-
-function Footer() {
+const Footer = () => {
     return (
-        <div>
-            <h3>Footer</h3>
-        </div>
+        <footer className="footer">
+            <div className="footer-container">
+
+                {/* Katalog Section */}
+                <div className="footer-column">
+                    <h3>КАТАЛОГ ТОВАРОВ</h3>
+                    <ul>
+                        <li><a href="#">КАРДИО ТРЕНАЖЕРЫ</a></li>
+                        <li><a href="#">COMPOSITE STRENGTH</a></li>
+                        <li><a href="#">TRUE STRETCH</a></li>
+                        <li><a href="#">САЙКЛИНГ</a></li>
+                        <li><a href="#">ГРУППОВЫЕ ТРЕНИРОВКИ</a></li>
+                        <li><a href="#">СИЛОВЫЕ ТРЕНИРОВКИ</a></li>
+                        <li><a href="#">КОНСОЛИ</a></li>
+                    </ul>
+                    <p className="copyright">© TRUE FITNESS</p>
+                </div>
+
+                {/* Informatsiya Section */}
+                <div className="footer-column">
+                    <h3>ИНФОРМАЦИЯ</h3>
+                    <ul>
+                        <li><a href="#">О БРЕНДЕ</a></li>
+                        <li><a href="#">ПРЕИМУЩЕСТВА</a></li>
+                        <li><a href="#">ОТКРЫТЬ КЛУБ</a></li>
+                        <li><a href="#">ПРОДУКЦИИ</a></li>
+                        <li><a href="#">КОНТАКТЫ</a></li>
+                    </ul>
+                </div>
+
+                {/* Kontaktlar Section */}
+                <div className="footer-column">
+                    <h3>КОНТАКТЫ</h3>
+                    <ul>
+                        <li><a href="#">ПОЛИТИКА КОНФИДЕНЦИАЛЬНОСТИ</a></li>
+                        <li><a href="#">КОНТАКТЫ</a></li>
+                    </ul>
+                </div>
+
+                {/* Podpiska Section */}
+                <div className="footer-column">
+                    <h3>ПОДПИСАТЬСЯ НА НОВОСТИ И АКЦИИ</h3>
+                    <div className="subscribe-box">
+                        <input type="email" placeholder="E - MAIL" />
+                        <button type="submit">➤</button>
+                    </div>
+                    <div className="social-icons">
+                        <a href="#"></a>
+                        <a href="#"></a>
+                        <a href="#"></a>
+                    </div>
+                </div>
+
+            </div>
+        </footer>
     );
-}
+};
 
 export default Footer;
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,11 +1,45 @@
-
 import './style.css'
+import logo from '../../assets/logo.png';
 
 function Header() {
     return (
         <div>
-            <h3>Header</h3>
-        </div>
+
+            <header className="header">
+                <div className="container header__container">
+                    <img src={logo} alt="logo" />
+                    <div className="header__nav">
+                        <div className='header__contact'>
+                            <select name="" id="">
+                                <option value="ru">RU</option>
+                            </select>
+                            <h2>+998 (90)-606-66-66</h2>
+                        </div>
+                        <button className='btn'>оставить заявку</button>
+                    </div>
+                </div>
+                <hr />
+
+                <nav className="header__menu">
+                    <ul>
+                        <li><a href="#brand">о бренде</a></li>
+                        <li><a href="#products">
+                            <select className='select' name="" id="">
+                                <option value="ru">продукции</option>
+                            </select></a></li>
+                        <li><a href="#advantages">преимущества</a></li>
+                        <li><a href="#open-club">открытый клуб</a></li>
+                        <li><a href="#contact">контакты</a></li>
+                    </ul>
+                </nav>
+            </header >
+
+
+
+
+        </div >
+
+
     );
 }
 
